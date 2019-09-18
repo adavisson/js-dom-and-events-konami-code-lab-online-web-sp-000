fcb619629995eb9de3b41af9cc9fc1ef889e9b49
Andrew Davisson <akdavisson4@gmail.com>
2019-09-18 14:58:29 -0600

    Done.

--- konami_code.js
+++ konami_code.js
@@ -3,22 +3,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
+  let body = document.querySelector('body');
+  body.addEventListener('keydown', konamiCodeHandler);
 
   function konamiCodeHandler(e) {
-    const key = parseInt(e.which);
+    const key = parseInt(e.detail || e.which || e.location);
     if (key === code[index]) {
       index++;
       if (index === code.length) {
-        alert("Hurray!");
+        alert("You passed the test!");
         index = 0;
       }
     } else {
       index = 0;
     }
   }
-
-  let body = document.querySelector('body');
-  body.addEventListener('keydown', konamiCodeHandler);
 }
 
 init();
